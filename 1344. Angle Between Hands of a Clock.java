3da098a3082527f20858ed3422917360edf2717e
Suhitha Reddy <suhithareddy2024@gmail.com>
2026-06-18 20:07:42 +0530

    Add files via upload

---
+++ 1344. Angle Between Hands of a Clock.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+      
+        double angle =Math.abs(30*hour-5.5 *minutes);
+        if(angle >180){
+            angle = 360- angle ;
+        }
+        return angle;
+        
+    }
+}
